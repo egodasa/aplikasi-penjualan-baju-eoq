8b77e8c67a0de3a734562142fb417d3fc5d04b0b
Ego Dafma Dasa <egodasa@gmail.com>
2020-06-23 22:19:56 +0700

    Perbaikan data supplier

--- Database/dbperabot.sql
+++ Database/dbperabot.sql
@@ -21,10 +21,10 @@ CREATE TABLE `barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `barang` (`kode_barang`, `nama_barang`, `harga_barang`, `stock`, `biaya_pesan`, `biaya_simpan`, `jml_kebutuhan`, `leadtime`, `stock_cadangan`, `satuan`) VALUES
-('B001',	'Lemari 2p Mahoni',	1900000,	5,	1200000,	500000,	70,	5,	3,	'Buah'),
-('B002',	'Sofa Arwana mahoni',	7800000,	5,	5400000,	1500000,	20,	3,	4,	'Buah'),
-('B003',	'Lemari 3p Jati',	4500000,	20,	3400000,	600000,	50,	4,	10,	'Buah'),
-('B004',	'Sofa kayu aldelwis ',	5800000,	7,	3600000,	1400000,	30,	3,	3,	'Buah');
+('BR00001',	'Lemari 2p Mahoni',	1900000,	5,	1200000,	500000,	70,	5,	3,	'Buah'),
+('BR00002',	'Sofa Arwana mahoni',	7800000,	5,	5400000,	1500000,	20,	3,	4,	'Buah'),
+('BR00003',	'Lemari 3p Jati',	4500000,	20,	3400000,	600000,	50,	4,	10,	'Buah'),
+('BR00004',	'Sofa kayu aldelwis ',	5800000,	7,	3600000,	1400000,	30,	3,	3,	'Buah');
 
 DROP TABLE IF EXISTS `detail_pembelian`;
 CREATE TABLE `detail_pembelian` (
@@ -41,16 +41,16 @@ CREATE TABLE `detail_pembelian` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `detail_pembelian` (`id_detail`, `no_nota`, `kode_barang`, `harga_barang`, `sub_total`, `jumlah`) VALUES
-(5,	'0101',	'B001',	2000,	12000,	2),
-(6,	'0101',	'B001',	2000,	12000,	2),
-(7,	'0101',	'B001',	2000,	12000,	2),
-(8,	'0101',	'B001',	2000,	12000,	2),
-(9,	'01010',	'B001',	111,	1111,	1),
-(10,	'01010',	'B001',	111,	1111,	1),
-(15,	'1012',	'B003',	2,	120,	1),
-(17,	'1001',	'B003',	3600000,	1080000000,	30),
-(18,	'1002',	'B004',	3600000,	720000000,	20),
-(19,	'1003',	'B001',	1200000,	24000000,	20);
+(5,	'0101',	'BR00001',	2000,	12000,	2),
+(6,	'0101',	'BR00001',	2000,	12000,	2),
+(7,	'0101',	'BR00001',	2000,	12000,	2),
+(8,	'0101',	'BR00001',	2000,	12000,	2),
+(9,	'01010',	'BR00001',	111,	1111,	1),
+(10,	'01010',	'BR00001',	111,	1111,	1),
+(15,	'1012',	'BR00003',	2,	120,	1),
+(17,	'1001',	'BR00003',	3600000,	1080000000,	30),
+(18,	'1002',	'BR00004',	3600000,	720000000,	20),
+(19,	'1003',	'BR00001',	1200000,	24000000,	20);
 
 DROP TABLE IF EXISTS `detail_penjualan`;
 CREATE TABLE `detail_penjualan` (
@@ -67,16 +67,16 @@ CREATE TABLE `detail_penjualan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `detail_penjualan` (`id_detail`, `no_nota`, `kode_barang`, `harga_barang`, `sub_total`, `jumlah`) VALUES
-(11,	'2306200208327259',	'B002',	7800000,	7800000,	1),
-(12,	'2306200208327259',	'B003',	4500000,	9000000,	2),
-(13,	'2306200209570345',	'B004',	5800000,	5800000,	1),
-(14,	'2306200210326594',	'B002',	7800000,	7800000,	1),
-(15,	'2306200211081925',	'B004',	5800000,	11600000,	2),
-(16,	'2306200238503299',	'B003',	4500000,	4500000,	1),
-(17,	'2306200239573587',	'B004',	5800000,	11600000,	2),
-(18,	'2306200240326523',	'B002',	7800000,	15600000,	2),
-(21,	'2306200242397563',	'B004',	5800000,	5800000,	1),
-(22,	'2306200242397563',	'B001',	1900000,	1900000,	1);
+(11,	'2306200208327259',	'BR00002',	7800000,	7800000,	1),
+(12,	'2306200208327259',	'BR00003',	4500000,	9000000,	2),
+(13,	'2306200209570345',	'BR00004',	5800000,	5800000,	1),
+(14,	'2306200210326594',	'BR00002',	7800000,	7800000,	1),
+(15,	'2306200211081925',	'BR00004',	5800000,	11600000,	2),
+(16,	'2306200238503299',	'BR00003',	4500000,	4500000,	1),
+(17,	'2306200239573587',	'BR00004',	5800000,	11600000,	2),
+(18,	'2306200240326523',	'BR00002',	7800000,	15600000,	2),
+(21,	'2306200242397563',	'BR00004',	5800000,	5800000,	1),
+(22,	'2306200242397563',	'BR00001',	1900000,	1900000,	1);
 
 DROP TABLE IF EXISTS `eoq`;
 CREATE TABLE `eoq` (
@@ -92,8 +92,8 @@ CREATE TABLE `eoq` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `eoq` (`kode_eoq`, `kode_barang`, `biaya_pesan`, `biaya_simpan`, `jml_kebutuhan`, `jml_eoq`, `frekuensi`) VALUES
-('E00001',	'B001',	1200000,	500000,	2,	547723,	15),
-('E00002',	'B002',	5400000,	1500000,	1300,	3096,	120);
+('E00001',	'BR00001',	1200000,	500000,	2,	547723,	15),
+('E00002',	'BR00002',	5400000,	1500000,	1300,	3096,	120);
 
 DROP TABLE IF EXISTS `pembelian`;
 CREATE TABLE `pembelian` (
@@ -107,9 +107,9 @@ CREATE TABLE `pembelian` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `pembelian` (`no_nota`, `tgl_beli`, `total_harga`, `kode_supplier`) VALUES
-('1001',	'2020-06-23 00:00:00',	1080000000,	'S002'),
-('1002',	'2020-06-01 00:00:00',	720000000,	'S002'),
-('1003',	'2020-06-23 00:00:00',	24000000,	'S003');
+('1001',	'2020-06-23 00:00:00',	1080000000,	'S00002'),
+('1002',	'2020-06-01 00:00:00',	720000000,	'S00002'),
+('1003',	'2020-06-23 00:00:00',	24000000,	'S00003');
 
 DROP TABLE IF EXISTS `pengguna`;
 CREATE TABLE `pengguna` (
@@ -123,7 +123,7 @@ CREATE TABLE `pengguna` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `pengguna` (`id_pengguna`, `username`, `password`, `level`, `nohp`, `alamat`) VALUES
-(1,	'karyawan',	'9e014682c94e0f2cc834bf7348bda428',	'Karyawan',	'',	''),
+(1,	'karyawan',	'9e014682c94e0f2cc834bf7348bda428',	'Karyawan',	'0812345',	'alamat 1'),
 (2,	'pimpinan',	'90973652b88fe07d05a4304f0a945de8',	'Pimpinan',	'',	'');
 
 DROP TABLE IF EXISTS `penjualan`;
@@ -156,8 +156,8 @@ CREATE TABLE `supplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `supplier` (`kode_supplier`, `nama_supplier`, `alamat`, `no_telp`) VALUES
-('S001',	'toko kurnia',	'Simpang tugu Siteba',	'081266838995'),
-('S002',	'Toko Anugrah',	'jln.Ligoo pasar raya',	'087895458901'),
-('S003',	'Okvanelti',	'Jln KP.Jua Lubuk Begalung ',	'081266879066');
+('S00001',	'toko kurnia',	'Simpang tugu Siteba',	'081266838995'),
+('S00002',	'Toko Anugrah',	'jln.Ligoo pasar raya',	'087895458901'),
+('S00003',	'Okvanelti',	'Jln KP.Jua Lubuk Begalung ',	'081266879066');
 
--- 2020-06-23 14:34:26
+-- 2020-06-23 15:19:00

8c6367ab0ebddab42a433729a27a066d8c070b36
egodasa <egodasa@gmail.com>
2020-08-11 10:38:06 +0700

    Penambahan trigger

--- Database/dbperabot.sql
+++ Database/dbperabot.sql
@@ -1,4 +1,4 @@
--- Adminer 4.7.5 MySQL dump
+-- Adminer 4.7.7 MySQL dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -21,10 +21,15 @@ CREATE TABLE `barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `barang` (`kode_barang`, `nama_barang`, `harga_barang`, `stock`, `biaya_pesan`, `biaya_simpan`, `jml_kebutuhan`, `leadtime`, `stock_cadangan`, `satuan`) VALUES
-('BR00001',	'Lemari 2p Mahoni',	1900000,	5,	1200000,	500000,	70,	5,	3,	'Buah'),
-('BR00002',	'Sofa Arwana mahoni',	7800000,	5,	5400000,	1500000,	20,	3,	4,	'Buah'),
-('BR00003',	'Lemari 3p Jati',	4500000,	20,	3400000,	600000,	50,	4,	10,	'Buah'),
-('BR00004',	'Sofa kayu aldelwis ',	5800000,	7,	3600000,	1400000,	30,	3,	3,	'Buah');
+('B001',	'Lemari 2p Mahoni',	1900000,	20,	50000,	100000,	70,	5,	20,	'Buah'),
+('B002',	'Sofa Arwana mahoni',	7800000,	5,	5400000,	1500000,	20,	3,	4,	'Buah'),
+('B003',	'Lemari 3p Jati',	4500000,	20,	3400000,	600000,	50,	4,	10,	'Buah'),
+('B004',	'Sofa kayu aldelwis ',	5800000,	7,	3600000,	1400000,	30,	3,	3,	'Buah'),
+('B005',	'lemari vintage',	2800000,	20,	2000000,	300000,	30,	3,	3,	'buah'),
+('B006',	'Lemari 3p mahoni coklat',	2000000,	5,	200000,	300000,	30,	2,	2,	'barang'),
+('BR007',	'fdsfds',	0,	0,	0,	0,	0,	21,	0,	'dsaasd'),
+('BR008',	'sadsad',	0,	0,	0,	0,	0,	12,	0,	'adsda'),
+('BR009',	'sasdsa',	0,	0,	0,	0,	0,	1121,	0,	'sasad');
 
 DROP TABLE IF EXISTS `detail_pembelian`;
 CREATE TABLE `detail_pembelian` (
@@ -36,21 +41,28 @@ CREATE TABLE `detail_pembelian` (
   `jumlah` int(11) NOT NULL,
   PRIMARY KEY (`id_detail`),
   KEY `no_nota` (`no_nota`),
-  KEY `kode_barang` (`kode_barang`),
-  CONSTRAINT `detail_pembelian_ibfk_3` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode_barang`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `kode_barang` (`kode_barang`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `detail_pembelian` (`id_detail`, `no_nota`, `kode_barang`, `harga_barang`, `sub_total`, `jumlah`) VALUES
-(5,	'0101',	'BR00001',	2000,	12000,	2),
-(6,	'0101',	'BR00001',	2000,	12000,	2),
-(7,	'0101',	'BR00001',	2000,	12000,	2),
-(8,	'0101',	'BR00001',	2000,	12000,	2),
-(9,	'01010',	'BR00001',	111,	1111,	1),
-(10,	'01010',	'BR00001',	111,	1111,	1),
-(15,	'1012',	'BR00003',	2,	120,	1),
-(17,	'1001',	'BR00003',	3600000,	1080000000,	30),
-(18,	'1002',	'BR00004',	3600000,	720000000,	20),
-(19,	'1003',	'BR00001',	1200000,	24000000,	20);
+(5,	'0101',	'B001',	2000,	12000,	2),
+(6,	'0101',	'B001',	2000,	12000,	2),
+(7,	'0101',	'B001',	2000,	12000,	2),
+(8,	'0101',	'B001',	2000,	12000,	2),
+(9,	'01010',	'B001',	111,	1111,	1),
+(10,	'01010',	'B001',	111,	1111,	1),
+(15,	'1012',	'B003',	2,	120,	1),
+(22,	'P001',	'B007',	3000000,	6000000,	2);
+
+DELIMITER ;;
+
+CREATE TRIGGER `tambahStokDariPembelian` BEFORE INSERT ON `detail_pembelian` FOR EACH ROW
+UPDATE barang SET stock = stock + NEW.jumlah WHERE kode_barang =  NEW.kode_barang;;
+
+CREATE TRIGGER `kurangiStokDariPembelian` AFTER DELETE ON `detail_pembelian` FOR EACH ROW
+UPDATE barang SET stock = stock - OLD.jumlah WHERE kode_barang = OLD.kode_barang;;
+
+DELIMITER ;
 
 DROP TABLE IF EXISTS `detail_penjualan`;
 CREATE TABLE `detail_penjualan` (
@@ -62,21 +74,22 @@ CREATE TABLE `detail_penjualan` (
   `jumlah` int(11) NOT NULL,
   PRIMARY KEY (`id_detail`),
   KEY `no_nota` (`no_nota`),
-  KEY `kode_barang` (`kode_barang`),
-  CONSTRAINT `detail_penjualan_ibfk_3` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode_barang`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `kode_barang` (`kode_barang`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `detail_penjualan` (`id_detail`, `no_nota`, `kode_barang`, `harga_barang`, `sub_total`, `jumlah`) VALUES
-(11,	'2306200208327259',	'BR00002',	7800000,	7800000,	1),
-(12,	'2306200208327259',	'BR00003',	4500000,	9000000,	2),
-(13,	'2306200209570345',	'BR00004',	5800000,	5800000,	1),
-(14,	'2306200210326594',	'BR00002',	7800000,	7800000,	1),
-(15,	'2306200211081925',	'BR00004',	5800000,	11600000,	2),
-(16,	'2306200238503299',	'BR00003',	4500000,	4500000,	1),
-(17,	'2306200239573587',	'BR00004',	5800000,	11600000,	2),
-(18,	'2306200240326523',	'BR00002',	7800000,	15600000,	2),
-(21,	'2306200242397563',	'BR00004',	5800000,	5800000,	1),
-(22,	'2306200242397563',	'BR00001',	1900000,	1900000,	1);
+(15,	'2306200211081925',	'B004',	5800000,	11600000,	2),
+(24,	'0108200438591166',	'B007',	3000000,	3000000,	1);
+
+DELIMITER ;;
+
+CREATE TRIGGER `kurangiStokDariPenjualan` BEFORE INSERT ON `detail_penjualan` FOR EACH ROW
+UPDATE barang SET stock = stock - NEW.jumlah WHERE kode_barang = NEW.kode_barang;;
+
+CREATE TRIGGER `tambahStokDariPenjualan` AFTER DELETE ON `detail_penjualan` FOR EACH ROW
+UPDATE barang SET stock = stock + OLD.jumlah WHERE kode_barang = OLD.kode_barang;;
+
+DELIMITER ;
 
 DROP TABLE IF EXISTS `eoq`;
 CREATE TABLE `eoq` (
@@ -87,13 +100,9 @@ CREATE TABLE `eoq` (
   `jml_kebutuhan` int(11) NOT NULL,
   `jml_eoq` decimal(10,0) NOT NULL,
   `frekuensi` decimal(10,0) NOT NULL,
-  KEY `kode_barang` (`kode_barang`),
-  CONSTRAINT `eoq_ibfk_2` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode_barang`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `kode_barang` (`kode_barang`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `eoq` (`kode_eoq`, `kode_barang`, `biaya_pesan`, `biaya_simpan`, `jml_kebutuhan`, `jml_eoq`, `frekuensi`) VALUES
-('E00001',	'BR00001',	1200000,	500000,	2,	547723,	15),
-('E00002',	'BR00002',	5400000,	1500000,	1300,	3096,	120);
 
 DROP TABLE IF EXISTS `pembelian`;
 CREATE TABLE `pembelian` (
@@ -102,14 +111,11 @@ CREATE TABLE `pembelian` (
   `total_harga` int(11) NOT NULL,
   `kode_supplier` varchar(20) NOT NULL,
   PRIMARY KEY (`no_nota`),
-  KEY `kode_supplier` (`kode_supplier`),
-  CONSTRAINT `pembelian_ibfk_2` FOREIGN KEY (`kode_supplier`) REFERENCES `supplier` (`kode_supplier`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `kode_supplier` (`kode_supplier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `pembelian` (`no_nota`, `tgl_beli`, `total_harga`, `kode_supplier`) VALUES
-('1001',	'2020-06-23 00:00:00',	1080000000,	'S00002'),
-('1002',	'2020-06-01 00:00:00',	720000000,	'S00002'),
-('1003',	'2020-06-23 00:00:00',	24000000,	'S00003');
+('P001',	'2020-08-01 00:00:00',	6000000,	'S004');
 
 DROP TABLE IF EXISTS `pengguna`;
 CREATE TABLE `pengguna` (
@@ -123,7 +129,7 @@ CREATE TABLE `pengguna` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `pengguna` (`id_pengguna`, `username`, `password`, `level`, `nohp`, `alamat`) VALUES
-(1,	'karyawan',	'9e014682c94e0f2cc834bf7348bda428',	'Karyawan',	'0812345',	'alamat 1'),
+(1,	'karyawan',	'9e014682c94e0f2cc834bf7348bda428',	'Karyawan',	'',	''),
 (2,	'pimpinan',	'90973652b88fe07d05a4304f0a945de8',	'Pimpinan',	'',	'');
 
 DROP TABLE IF EXISTS `penjualan`;
@@ -137,14 +143,8 @@ CREATE TABLE `penjualan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `penjualan` (`no_nota`, `tgl_jual`, `nm_pembeli`, `uraian_jual`, `total_harga`) VALUES
-('2306200208327259',	'2020-06-23 00:00:00',	'buk tika',	'customer',	16800000),
-('2306200209570345',	'2020-06-23 00:00:00',	'pak dodi',	'customer',	5800000),
-('2306200210326594',	'2020-06-23 00:00:00',	'bpk Jon ',	'customer',	7800000),
-('2306200234268993',	'2020-06-09 00:00:00',	'pak saiful',	'customer',	0),
-('2306200238503299',	'2020-06-05 00:00:00',	'bpk saiful',	'customer',	4500000),
-('2306200239573587',	'2020-05-19 00:00:00',	'buk sati',	'customer',	11600000),
-('2306200240326523',	'2020-04-20 00:00:00',	'pak anton',	'customer',	15600000),
-('2306200242397563',	'2020-05-03 00:00:00',	'buk safitri',	'customer',	7700000);
+('0108200438591166',	'2020-08-01 00:00:00',	'BUK ANI',	'CUSTOMER',	3000000),
+('2306200234268993',	'2020-06-09 00:00:00',	'pak saiful',	'customer',	0);
 
 DROP TABLE IF EXISTS `supplier`;
 CREATE TABLE `supplier` (
@@ -156,8 +156,10 @@ CREATE TABLE `supplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `supplier` (`kode_supplier`, `nama_supplier`, `alamat`, `no_telp`) VALUES
-('S00001',	'toko kurnia',	'Simpang tugu Siteba',	'081266838995'),
-('S00002',	'Toko Anugrah',	'jln.Ligoo pasar raya',	'087895458901'),
-('S00003',	'Okvanelti',	'Jln KP.Jua Lubuk Begalung ',	'081266879066');
+('S001',	'toko kurnia',	'Simpang tugu Siteba',	'081266838995'),
+('S002',	'Toko Anugrah',	'jln.Ligoo pasar raya',	'087895458901'),
+('S003',	'Okvanelti',	'Jln KP.Jua Lubuk Begalung ',	'081266879066'),
+('S004',	'toko cami',	'padang ',	'081266775644'),
+('S005',	'toko amri furniture',	'bukittinngi',	'081266889976');
 
--- 2020-06-23 15:19:00
+-- 2020-08-11 03:35:11
